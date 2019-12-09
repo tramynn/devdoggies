acbb95e0619275c55b8859f733fd100b23786704
Tramy Nguyen <tramynn2@gmail.com>
2019-12-08 19:12:38 -0600

    set up auth reducer

---
+++ src/redux/reducers/authReducer.js
@@ -0,0 +1,116 @@
+import Axios from "axios";
+
+const initialState = {
+  user: {},
+  user_id: null,
+  first_name: "",
+  last_name: "",
+  age: null,
+  email: "",
+  username: "",
+  password: "",
+  loading: false
+};
+
+const GET_SESSION = "GET_SESSION";
+const REGISTER_USER = "REGISTER_USER";
+const LOGIN_USER = "LOGIN_USER";
+const LOGOUT_USER = "LOGOUT_USER";
+
+export function getSession() {
+  return {
+    type: GET_SESSION,
+    payload: Axios.get("/auth/user")
+  };
+}
+
+export function registerUser(newUser) {
+  return {
+    type: REGISTER_USER,
+    payload: Axios.post("/auth/register", newUser)
+  };
+}
+
+export function loginUser(user) {
+  return {
+    type: LOGIN_USER,
+    payload: Axios.post("/auth/login", user)
+  };
+}
+
+export function logoutUser() {
+  Axios.post("/auth/logout");
+  return {
+    type: LOGOUT_USER
+  };
+}
+
+export default function reducer(state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case `${GET_SESSION}_PENDING`: {
+      return {
+        ...state,
+        loading: true
+      };
+    }
+    case `${GET_SESSION}_FULFILLED`: {
+      return {
+        ...state,
+        user_id: payload.data.user_id,
+        username: payload.data.username,
+        first_name: payload.data.first_name,
+        last_name: payload.data.last_name,
+        age: payload.data.age,
+        email: payload.data.email
+      };
+    }
+    case `${REGISTER_USER}_PENDING`: {
+      return {
+        ...state,
+        loading: true
+      };
+    }
+    case `${REGISTER_USER}_FULFILLED`: {
+      return {
+        ...state,
+        user_id: payload.data.user_id,
+        username: payload.data.username,
+        first_name: payload.data.first_name,
+        last_name: payload.data.last_name,
+        age: payload.data.age,
+        email: payload.data.email
+      };
+    }
+    case `${LOGIN_USER}_PENDING`: {
+      return {
+        ...state,
+        loading: true
+      };
+    }
+    case `${LOGIN_USER}_FULFILLED`: {
+      return {
+        ...state,
+        loading: false,
+        username: payload.data.username,
+        password: payload.data.password
+      };
+    }
+    case LOGOUT_USER: {
+      return {
+        user: {},
+        user_id: null,
+        first_name: "",
+        last_name: "",
+        age: null,
+        email: "",
+        username: "",
+        password: "",
+        loading: false
+      };
+    }
+    default:
+      return state;
+  }
+}
