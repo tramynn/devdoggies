f0a420a60b740eab782858e4aef54166c840ef69
Tramy Nguyen <tramynn2@gmail.com>
2020-01-03 12:31:21 -0600

    initialized some components

---
+++ src/Components/Guide/Guide.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Guide = () => {
+  <div>
+    <h1>Guide</h1>
+  </div>
+}
+
+export default Guide;
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -8,9 +8,9 @@ class Register extends Component {
 
   render() {
     return (
-
-      <div><h1>Register</h1></div>
-
+      <div>
+        <h1>Register</h1>
+      </div>
     );
   }
 }
